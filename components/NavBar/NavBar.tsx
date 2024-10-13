062036879197537fd8604c6f48c0170092b0d61f
craks31 <prgcf@umkc.edu>
2024-10-13 11:05:08 -0500

    home page

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,21 +1,46 @@
-"use client"; 
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaHome } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
 
 const NavBar = () => {
-
-  //const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="navbar z-10 realtive flex justify-around items-center p-4 mx-auto">
-       <h1 className="uppercase text-lg whitespace-nowrap p-0">Sai Rakesh Pokala</h1>
-      <ul className="nav-menu flex space-x-8 tracking-widest text-xs text-center text-blue-100 max-w-80">
-      <li className="flex items-center space-x-2">
+      <h1 className="text-red-500 text-2xl font-extrabold tracking-wide drop-shadow-lg custom-first-letter">
+        Rakesh Pokala
+      </h1>
+      {/* Mobile Hamburger Menu */}
+      <div className="md:hidden">
+        <button className="text-blue-100 focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+          <FaBars className="w-6 h-6" />
+        </button>
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isOpen && (
+      <div className="md:hidden z-10 absolute top-16 shadow-md">
+        <ul className="flex flex-col space-y-4 p-4 text-center">
+          <li className="flex items-center justify-center space-x-2">
+            <FaHome /> <span>Home</span>
+          </li>
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      )}
+
+      {/* Full Nav Menu for Desktop */}
+      <ul className="nav-menu hidden md:flex space-x-8 tracking-widest text-xs text-center text-blue-100">
+        <li className="flex items-center space-x-2">
           <FaHome /> <span>Home</span>
         </li>
         <li className="whitespace-nowrap">About me</li>
         <li className="whitespace-nowrap">Projects</li>
-        <li>Skills</li>
-        <li>Contact</li>
+        <li className="whitespace-nowrap">Skills</li>
+        <li className="whitespace-nowrap">Contact</li>
       </ul>
     </div>
   );
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -13,10 +13,10 @@ const MagicButton = ({
     otherClasses?: string;
 }) => {
     return (
-        <button className="p-[3px] relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-red-400 to-orange-900 rounded-lg" />
+        <button className="p-[1px] w-full max-w-md relative">
+            <div className="absolute inset-0 bg-gradient-to-r from-red-400 to-orange-800 rounded-lg" />
             <div className={`inline-flex h-full w-full cursor-pointer items-center justify-center
-             bg-slate-950 px-7 rounded-lg  relative group transition duration-200 text-white hover:bg-transparent gap-2 ${otherClasses}`}>
+             bg-slate-950 px-7 rounded-lg  relative group transition duration-200 ease-in-out text-white hover:bg-transparent gap-2 ${otherClasses}`}>
                 {position === "left" && icon}
                 {title}
                 {position === "right" && icon}
